475175eed59d7d038d4326e7c1ef33ae13ac08e7
Myk Melez <myk@mykzilla.org>
2018-04-16 13:44:43 -0700

    move test function into xulstore crate
    MozReview-Commit-ID: 4N46qm1lqt1

--- toolkit/components/xulstore/lib.rs
+++ toolkit/components/xulstore/lib.rs
@@ -1,1 +1,8 @@
 extern crate rkv;
+
+#[no_mangle]
+pub extern fn test_xul_store() -> *const u8 {
+    // NB: rust &str aren't null terminated.
+    let greeting = "hello from XUL store.\0";
+    greeting.as_ptr()
+}
--- toolkit/library/gtest/rust/lib.rs
+++ toolkit/library/gtest/rust/lib.rs
@@ -6,4 +6,3 @@ extern crate gkrust_shared;
 extern crate mp4parse_gtest;
 extern crate nsstring_gtest;
 extern crate xpcom_gtest;
-extern crate xulstore_gtest;
