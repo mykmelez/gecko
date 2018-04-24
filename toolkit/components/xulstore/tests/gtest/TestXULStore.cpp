16cc99cc55737614a77b065d584d66e016965956
Myk Melez <myk@mykzilla.org>
2018-04-24 16:14:36 -0700

    clean up tests further
    MozReview-Commit-ID: Gk25T8Uy700

--- toolkit/components/xulstore/tests/gtest/TestXULStore.cpp
+++ toolkit/components/xulstore/tests/gtest/TestXULStore.cpp
@@ -3,31 +3,42 @@
 #include "nsString.h"
 
 extern "C" nsresult xulstore_set_value(nsAString* doc, nsAString* id, nsAString* attr, nsAString* value);
+extern "C" bool xulstore_has_value(nsAString* doc, nsAString* id, nsAString* attr);
 extern "C" void xulstore_get_value(const nsAString* doc, const nsAString* id, const nsAString* attr, nsAString* value);
+
 TEST(XULStore, SetGetValue) {
-  nsAutoString doc(NS_LITERAL_STRING("chrome://browser/content/example.xul"));
-  nsAutoString id(NS_LITERAL_STRING("window"));
-  nsAutoString attr(NS_LITERAL_STRING("width"));
+  nsAutoString doc(NS_LITERAL_STRING("SetGetValue"));
+  nsAutoString id(NS_LITERAL_STRING("foo"));
+  nsAutoString attr(NS_LITERAL_STRING("bar"));
 
   {
-    nsAutoString value(NS_LITERAL_STRING("800"));
+    nsAutoString value(NS_LITERAL_STRING("baz"));
     EXPECT_EQ(xulstore_set_value(&doc, &id, &attr, &value), NS_OK);
   }
 
   {
     nsAutoString value;
+    EXPECT_TRUE(value.EqualsASCII(""));
     xulstore_get_value(&doc, &id, &attr, &value);
-    EXPECT_TRUE(value.EqualsASCII("800"));
+    EXPECT_TRUE(value.EqualsASCII("baz"));
   }
 }
 
-extern "C" bool xulstore_has_value(nsAString* doc, nsAString* id, nsAString* attr);
 TEST(XULStore, HasValue) {
-  nsAutoString doc(NS_LITERAL_STRING("chrome://browser/content/has-value.xul"));
-  nsAutoString id(NS_LITERAL_STRING("window"));
-  nsAutoString attr(NS_LITERAL_STRING("height"));
+  nsAutoString doc(NS_LITERAL_STRING("HasValue"));
+  nsAutoString id(NS_LITERAL_STRING("foo"));
+  nsAutoString attr(NS_LITERAL_STRING("bar"));
   EXPECT_FALSE(xulstore_has_value(&doc, &id, &attr));
-  nsAutoString value(NS_LITERAL_STRING("600"));
+  nsAutoString value(NS_LITERAL_STRING("baz"));
   EXPECT_EQ(xulstore_set_value(&doc, &id, &attr, &value), NS_OK);
   EXPECT_TRUE(xulstore_has_value(&doc, &id, &attr));
+}
+
+TEST(XULStore, GetMissingValue) {
+  nsAutoString doc(NS_LITERAL_STRING("GetMissingValue"));
+  nsAutoString id(NS_LITERAL_STRING("foo"));
+  nsAutoString attr(NS_LITERAL_STRING("bar"));
+  nsAutoString value;
+  xulstore_get_value(&doc, &id, &attr, &value);
+  EXPECT_TRUE(value.EqualsASCII(""));
 }
