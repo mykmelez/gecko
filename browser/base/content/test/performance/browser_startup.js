abc774a24d78b3defbd27344fe9e47fc81589d0d
Myk Melez <myk@mykzilla.org>
2019-04-29 17:59:28 -0700

    whitelist old XULStore.jsm

--- browser/base/content/test/performance/browser_startup.js
+++ browser/base/content/test/performance/browser_startup.js
@@ -119,7 +119,11 @@ const startupPhases = {
 if (Services.prefs.getBoolPref("browser.startup.blankWindow") &&
     Services.prefs.getCharPref("extensions.activeThemeID", "default-theme@mozilla.org") ==
       "default-theme@mozilla.org") {
-  startupPhases["before profile selection"].whitelist.modules.add("resource://gre/modules/XULStoreNew.jsm");
+  if (AppConstants.MOZ_NEW_XULSTORE) {
+    startupPhases["before profile selection"].whitelist.modules.add("resource://gre/modules/XULStoreNew.jsm");
+  } else {
+    startupPhases["before profile selection"].whitelist.modules.add("resource://gre/modules/XULStore.jsm");
+  }
 }
 
 if (!gBrowser.selectedBrowser.isRemoteBrowser) {
